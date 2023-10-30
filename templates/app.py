0b9ca6fd396b73e75c812cf03580121623994d74
caiquendp <148247223+caiquendp@users.noreply.github.com>
2023-10-30 12:15:19 +0000

    segundo

--- templates/app.py
+++ templates/app.py
@@ -3,9 +3,9 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  nome = None
-  if request.method == 'POST' and 'nome' in request.form:
-    nome = request.form['nome']
-  return render_template('index.html', nome=nome)
+    nome = None
+    if request.method == 'POST' and 'nome' in request.form:
+        nome = request.form['nome']
+    return render_template('index.html', nome=nome)
 
 app.run(debug=True)
